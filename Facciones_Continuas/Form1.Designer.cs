888fd3a77e7b30228d8bcaea0ed97df15b906f86
Chuy Arellano <LIS22110288@irapuato.tecnm.mx>
2023-09-19 19:44:16 -0600

    Agregar archivos de proyecto.

---
+++ Facciones_Continuas/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Facciones_Continuas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtNum1 = new System.Windows.Forms.TextBox();
+            this.TxtNum2 = new System.Windows.Forms.TextBox();
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.TablaDatos = new System.Windows.Forms.DataGridView();
+            this.ColumnPaso = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnOp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnEc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.TablaDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtNum1
+            // 
+            this.TxtNum1.Location = new System.Drawing.Point(152, 66);
+            this.TxtNum1.Name = "TxtNum1";
+            this.TxtNum1.Size = new System.Drawing.Size(100, 20);
+            this.TxtNum1.TabIndex = 1;
+            this.TxtNum1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtNum1_KeyPress);
+            // 
+            // TxtNum2
+            // 
+            this.TxtNum2.Location = new System.Drawing.Point(152, 127);
+            this.TxtNum2.Name = "TxtNum2";
+            this.TxtNum2.Size = new System.Drawing.Size(100, 20);
+            this.TxtNum2.TabIndex = 2;
+            // 
+            // BtnCalcular
+            // 
+            this.BtnCalcular.Location = new System.Drawing.Point(388, 102);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.BtnCalcular.TabIndex = 3;
+            this.BtnCalcular.Text = "Calcular";
+            this.BtnCalcular.UseVisualStyleBackColor = true;
+            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // TablaDatos
+            // 
+            this.TablaDatos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.TablaDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TablaDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnPaso,
+            this.ColumnOp,
+            this.ColumnEc});
+            this.TablaDatos.Enabled = false;
+            this.TablaDatos.Location = new System.Drawing.Point(97, 220);
+            this.TablaDatos.Name = "TablaDatos";
+            this.TablaDatos.Size = new System.Drawing.Size(628, 192);
+            this.TablaDatos.TabIndex = 4;
+            // 
+            // ColumnPaso
+            // 
+            this.ColumnPaso.FillWeight = 30.45685F;
+            this.ColumnPaso.HeaderText = "Paso";
+            this.ColumnPaso.Name = "ColumnPaso";
+            // 
+            // ColumnOp
+            // 
+            this.ColumnOp.FillWeight = 134.7716F;
+            this.ColumnOp.HeaderText = "Operacion";
+            this.ColumnOp.Name = "ColumnOp";
+            // 
+            // ColumnEc
+            // 
+            this.ColumnEc.FillWeight = 134.7716F;
+            this.ColumnEc.HeaderText = "Ecuacion";
+            this.ColumnEc.Name = "ColumnEc";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "1er Numero:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "2do Numero:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(291, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Fracciones Continuas";
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Location = new System.Drawing.Point(525, 102);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.BtnLimpiar.TabIndex = 8;
+            this.BtnLimpiar.Text = "Limpiar";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TablaDatos);
+            this.Controls.Add(this.BtnCalcular);
+            this.Controls.Add(this.TxtNum2);
+            this.Controls.Add(this.TxtNum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.TablaDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox TxtNum1;
+        private System.Windows.Forms.TextBox TxtNum2;
+        private System.Windows.Forms.Button BtnCalcular;
+        private System.Windows.Forms.DataGridView TablaDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnPaso;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnOp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnEc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BtnLimpiar;
+    }
+}
+
---
+++ Facciones_Continuas/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Facciones_Continuas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            int a, b, i = 1;
+            a = int.Parse(TxtNum1.Text);
+            b = int.Parse(TxtNum2.Text);
+
+            while (b != 0)
+            {
+                
+                int temp = b;
+                b = a%b;
+                if (b == 0)
+                {
+                    TablaDatos.Rows.Add(i, a + " divido entre " + temp + " es " + (a / temp) + " y sobran " + b, a + " / " + temp + " = " + (a / temp));
+                }
+                else
+                {
+                    TablaDatos.Rows.Add(i, a + " divido entre " + temp + " es " + (a / temp) + " y sobran " + b, a + " / " + temp + " = " + (a / temp) + " + 1 / " + temp + " / " + b);
+                }
+                
+                a = temp;
+                i++;
+            }
+            
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            TxtNum1.Text = "";
+            TxtNum2.Text = "";
+            TablaDatos.Rows.Clear();
+            TxtNum1.Focus();
+        }
+
+        private void TxtNum1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                TxtNum2.Focus();
+            }
+        }
+
+    }
+}
