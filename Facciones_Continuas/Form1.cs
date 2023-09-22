53406af407bcf61e1c016b7d6ad9a8247cd7c765
Jesus-Oswaldo-Arellano-Marin <144375889+chuyArellano@users.noreply.github.com>
2023-09-21 23:43:58 -0600

    Form1.cs

--- Facciones_Continuas/Form1.cs
+++ Facciones_Continuas/Form1.cs
@@ -20,26 +20,36 @@ namespace Facciones_Continuas
 
         private void BtnCalcular_Click(object sender, EventArgs e)
         {
-            int a, b, i = 1;
-            a = int.Parse(TxtNum1.Text);
-            b = int.Parse(TxtNum2.Text);
+            string ecuacion = "";
+ int a, b, i = 1;
+ a = int.Parse(TxtNum1.Text);
+ b = int.Parse(TxtNum2.Text);
 
-            while (b != 0)
-            {
-                
-                int temp = b;
-                b = a%b;
-                if (b == 0)
-                {
-                    TablaDatos.Rows.Add(i, a + " divido entre " + temp + " es " + (a / temp) + " y sobran " + b, a + " / " + temp + " = " + (a / temp));
-                }
-                else
-                {
-                    TablaDatos.Rows.Add(i, a + " divido entre " + temp + " es " + (a / temp) + " y sobran " + b, a + " / " + temp + " = " + (a / temp) + " + 1 / " + temp + " / " + b);
-                }
-                
-                a = temp;
-                i++;
+ void CalcularEcuacion(int x, int y)
+ {
+     if (y == 0)
+     {
+         ecuacion = a + "/" + x + " = " + (a / x);
+     }
+     else
+     {
+         int temp = y;
+         y = x % y;
+         if (y == 0)
+         {
+             ecuacion = a + "/" + temp + " = " + (a / temp);
+         }
+         else
+         {
+             ecuacion = a + "/" + temp + " = " + (a / temp) + " + 1/" + temp + " / " + y;
+         }
+         TablaDatos.Rows.Add(i, a + " divido entre " + temp + " es " + (a / temp) + " y sobran " + y, ecuacion);
+         a = temp;
+         i++;
+         CalcularEcuacion(a, y); // Llamada recursiva
+     }
+ }
+ CalcularEcuacion(a, b);
             }
             
         }
